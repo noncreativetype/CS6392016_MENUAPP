15319ee25a9dc9c3986b239ac3cc20354cebacf5
Fabian Patino <frpatino@gmail.com>
2016-06-17 15:59:05 -0400

    added text message

--- app/src/main/java/com/example/fabianpatino/menuapp/MainActivity.java
+++ app/src/main/java/com/example/fabianpatino/menuapp/MainActivity.java
@@ -2,12 +2,56 @@ package com.example.fabianpatino.menuapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Bundle;
+import android.app.Activity;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button sendBtn;
+    public String txtphoneNo = "9142991173";
+    String txtMessage = "hello";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sendBtn = (Button) findViewById(R.id.btnSendSMS);
+      // txtphoneNo = 9142991173 ;
+//        txtMessage = (EditText) findViewById(R.id.editText2);
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                sendSMSMessage();
+            }
+        });
     }
-}
+    protected void sendSMSMessage() {
+        Log.i("Send SMS", "");
+        String phoneNo = txtphoneNo;
+        String message = txtMessage;
+
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo, null, message, null, null);
+            Toast.makeText(getApplicationContext(), "SMS sent.", Toast.LENGTH_LONG).show();
+        }
+
+        catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "SMS faild, please try again.", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+
+   // @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+
+    }
+
