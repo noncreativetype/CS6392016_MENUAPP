36622bb683c86a62675b9ad854d3c5203160f4f3
Fabian Patino <frpatino@gmail.com>
2016-06-17 20:37:05 -0400

    added all the activities

--- app/src/main/java/com/example/fabianpatino/menuapp/MainActivity.java
+++ app/src/main/java/com/example/fabianpatino/menuapp/MainActivity.java
@@ -16,7 +16,7 @@ import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
     Button sendBtn;
-    Button button2;
+    Button button2,button3,button4,button5,button6;
 
     public String txtphoneNo = "9142991173";
     String txtMessage = "hello";
@@ -26,41 +26,91 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sendBtn = (Button) findViewById(R.id.btnSendSMS);
-        button2 = (Button) findViewById(R.id.button2);
-        call();
+        button2 = (Button) findViewById(R.id.button2);//Phone – When we click on Phone, the default Android caller app is displayed. The phone number is populated with a phone number of your choice.
+        button3 = (Button) findViewById(R.id.button3);//Web – When we click on Web, the browser opens up with a URL of your choice.
+        button4 = (Button) findViewById(R.id.button4);//Map – When we click on Map, the Google Maps app opens and it is centered on your birth place.
+        button5 = (Button) findViewById(R.id.button5);//Share – When we click on Share, the chooser Activity opens with “Share the love” as a title.
+        button6 = (Button) findViewById(R.id.button6);//New Activity – A new activity (NewACtivity.java) opens up when we click on the New Activity button. The textview in the activity will be: Hello <your first name, last name>.
+
 
         sendBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 sendSMSMessage();
             }
+        }
+        );
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                call();
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                web();
+            }
+            });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                map();
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                share();
+            }
+        });
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                newAct();
+            }
         });
     }
+
         private void call() {
-            Intent in=new Intent(Intent.ACTION_CALL,Uri.parse("9142991173"));
-            try{
-                startActivity(in);
-            }
-
-            catch (android.content.ActivityNotFoundException ex){
-                Toast.makeText(getApplicationContext(),"yourActivity is not founded",Toast.LENGTH_SHORT).show();
-            }
+            final Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse("tel:914-299-1173"));
+            startActivity(intent);
         }
-
 ///////////////////////////////
     protected void sendSMSMessage() {
         Log.i("Send SMS", "");
         String phoneNo = txtphoneNo;
         String message = txtMessage;
+        SmsManager smsManager = SmsManager.getDefault();
+//            smsManager.sendTextMessage(phoneNo, null, message, null, null);
+        Toast.makeText(getApplicationContext(), "SMS sent.", Toast.LENGTH_LONG).show();
+        final Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("smsto: " + Uri.encode(phoneNo)));
+        intent.putExtra("sms_body", message);
+        startActivity(intent);
 
-        try {
-            SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage(phoneNo, null, message, null, null);
-            Toast.makeText(getApplicationContext(), "SMS sent.", Toast.LENGTH_LONG).show();
-        }
+    }
+    private void web() {
 
-        catch (Exception e) {
-            Toast.makeText(getApplicationContext(), "SMS faild, please try again.", Toast.LENGTH_LONG).show();
-            e.printStackTrace();
-        }
+        String url = "http://www.artbrut.org.uk";
+        Intent setWeb = new Intent(Intent.ACTION_VIEW);
+        setWeb.setData(Uri.parse(url));
+        startActivity(setWeb);
+    }
+    private void map() {
+        String geoLocation= "geo:-2.894997,-79.0301574";
+        Intent locate = new Intent(Intent.ACTION_VIEW);
+        locate.setData(Uri.parse(geoLocation));
+        startActivity(locate);
+    }
+    private void share() {
+        Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND);
+        shareIntent.setType("url");
+        startActivity(Intent.createChooser(shareIntent,"Share the Love"));
+
+    }
+    private void newAct() {
+       startActivity( new Intent(MainActivity.this,New_activity.class));
+
     }
 }
